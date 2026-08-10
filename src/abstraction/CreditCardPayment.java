7729f0537b6aa8f02c1695cbef1c36eacce8a7fb
Anucool <bagaleanucool55@gmail.com>
2026-08-10 20:08:22 +0100

    abstraction practice

--- src/abstraction/CreditCardPayment.java
+++ src/abstraction/CreditCardPayment.java
@@ -10,7 +10,7 @@ public class CreditCardPayment extends Payment {
     @Override
     public void processPayment() {
         System.out.println("Processing $" + amount + " using Credit Card: " + cardNumber);
-        
+        System.out.println("Payment Successful");
     }
 
 	
