77e1ce22e68afd12987c46f17ecfb119c137e271
Anucool <bagaleanucool55@gmail.com>
2026-08-13 21:39:37 +0100

    abstraction practice

--- src/abstraction/Main.java
+++ src/abstraction/Main.java
@@ -5,6 +5,7 @@ public class Main {
         
         Payment payment1 = new CreditCardPayment(150.00, "1234-5678-9012");
         Payment payment2 = new PayPalPayment(45.50, "user@gmail.com");
+        Payment payment3 = new CreditCardPayment(50,"0000-0000-0000");
 
         
         payment1.processPayment();
@@ -14,5 +15,11 @@ public class Main {
 
         payment2.processPayment();
         payment2.printReceipt();
+        
+        System.out.println("-------------------");
+
+        payment3.processPayment();
+        payment3.printReceipt();
     }
+    
 }
