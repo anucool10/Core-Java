04315ac1dc56eb217bfb1477fca6a3bce4a41db6
Anucool <bagaleanucool55@gmail.com>
2026-08-17 19:03:25 +0100

    hashset practice

---
+++ src/collectionframework/Hashset.java
@@ -0,0 +1,22 @@
+package collectionframework;
+
+import java.util.HashSet;
+
+public class Hashset {
+	public static void main(String args[]) {
+		HashSet<String> cars = new HashSet<String>();
+		cars.add("BMW");
+		cars.add("Toyota");
+		cars.add("Hyundai");
+		cars.add("Mercedes");
+		cars.add("BMW");
+		
+		System.out.println(cars.contains("BMW"));
+		
+		System.out.println(cars);
+		
+	}
+
+	
+
+}
