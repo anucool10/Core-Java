99a9ca5fed9fc7dba075d15a939eeb13f4da496a
Anucool <bagaleanucool55@gmail.com>
2026-08-26 23:08:27 +0100

    QUEUE practice

---
+++ src/collectionframework/QueuePractice.java
@@ -0,0 +1,21 @@
+package collectionframework;
+
+import java.util.LinkedList;
+
+public class QueuePractice {
+
+	public static void main(String[] args) {
+		LinkedList<String>ticketLine = new LinkedList<>();
+		ticketLine.add("Alex");
+		ticketLine.add("Bob");
+		ticketLine.add("Charlie");
+		ticketLine.add("Daisy");
+		System.out.println("Current Line: " + ticketLine);
+		System.out.println("Next up: " + ticketLine.peek());
+		String served = ticketLine.remove();
+        System.out.println("Now serving: " + served);
+        System.out.println("Line after serving: " + ticketLine);
+	}
+	
+
+}
