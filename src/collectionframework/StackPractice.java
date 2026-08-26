78890720de8fd9009d25fd48398cc38a7c3de7c6
Anucool <bagaleanucool55@gmail.com>
2026-08-26 23:02:07 +0100

    stack practice

---
+++ src/collectionframework/StackPractice.java
@@ -0,0 +1,22 @@
+package collectionframework;
+
+import java.util.Stack;
+
+public class StackPractice {
+	public static void main (String[] args) {
+		Stack<String>history = new Stack<>();
+		history.push("google.com");
+		history.push("youtube.com");
+		history.push("github.com");
+		
+		System.out.println("Current Stack: " + history);
+		String removedPage = history.pop();
+        System.out.println("Just left: " + removedPage);
+		
+		System.out.println("Stack after going back: "+history);
+	}
+
+	
+	
+	
+}
