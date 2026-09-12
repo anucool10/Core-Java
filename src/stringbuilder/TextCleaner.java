22227603b6c3db2f9b90e98c30663388aa7b0b00
Anucool <bagaleanucool55@gmail.com>
2026-09-12 22:12:18 +0100

    clean string using stringbuilder

--- src/stringbuilder/TextCleaner.java
+++ src/stringbuilder/TextCleaner.java
@@ -5,7 +5,7 @@ import java.util.Scanner;
 public class TextCleaner {
 	public static void main(String[] args) {
 		Scanner input = new Scanner(System.in);
-		System.out.println("Enter text: ");
+		System.out.println("Enter text here: ");
 		String userInput = input.nextLine();
 		StringBuilder clean = new StringBuilder();
 		for(int i = 0; i<userInput.length();i++) {
